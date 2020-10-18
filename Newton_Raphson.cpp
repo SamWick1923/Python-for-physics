d09b4207729494207360f312e7c5c40939ddb0d4
Swarali-glitch <71065651+Swarali-glitch@users.noreply.github.com>
2020-10-18 21:03:59 +0530

    Update Newton_Raphson.cpp

--- Newton_Raphson.cpp
+++ Newton_Raphson.cpp
@@ -2,20 +2,16 @@
 #define EPSILON 0.001 
 using namespace std; 
   
-// An example function whose solution is determined using 
-// Bisection Method. The function is x^3 - x^2  + 2 
 double func(double x) 
 { 
     return x*x*x - x*x + 2; 
 } 
   
-// Derivative of the above function which is 3*x^x - 2*x 
 double derivFunc(double x) 
 { 
     return 3*x*x - 2*x; 
 } 
   
-// Function to find the root 
 void newtonRaphson(double x) 
 { 
     double h = func(x) / derivFunc(x); 
@@ -23,17 +19,15 @@ void newtonRaphson(double x)
     { 
         h = func(x)/derivFunc(x); 
    
-        // x(i+1) = x(i) - f(x) / f'(x)   
         x = x - h; 
     } 
   
     cout << "The value of the root is : " << x; 
 } 
   
-// Driver program to test above 
 int main() 
 { 
-    double x0 = -20; // Initial values assumed 
+    double x0 = -20; 
     newtonRaphson(x0); 
     return 0; 
 } 
